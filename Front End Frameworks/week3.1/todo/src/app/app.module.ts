6d7a5fea39e2e47a1ac56a8bf710d7204e5f2842
DimitarBelchev <dimitarbelchev@abv.bg>
2020-02-11 13:23:53 +0200

    commit 3

--- Front End Frameworks/week3.1/todo/src/app/app.module.ts
+++ Front End Frameworks/week3.1/todo/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { TodoModule } from './todo/todo.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    AppRoutingModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
