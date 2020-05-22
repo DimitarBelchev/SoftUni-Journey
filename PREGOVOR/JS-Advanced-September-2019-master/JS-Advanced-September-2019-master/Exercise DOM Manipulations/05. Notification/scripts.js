8c8882c7531e258c5e4a389f005ac0fb7b1c8627
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2020-05-22 23:07:28 +0300

    //

--- PREGOVOR/JS-Advanced-September-2019-master/JS-Advanced-September-2019-master/Exercise DOM Manipulations/05. Notification/scripts.js
+++
@@ -1,6 +0,0 @@
-function notify(message) {
-    const div = document.getElementById('notification');
-    div.textContent = message;
-    div.style.display = 'block';
-    setTimeout(function(){ div.style.display = 'none';}, 2000);
-}
